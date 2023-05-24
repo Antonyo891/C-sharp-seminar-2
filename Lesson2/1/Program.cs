0dec1153d0a3250dea817088b20840fe5dc7973e
Anton <B_atonyo_S@bk.ru>
2023-05-24 22:47:23 +0300

    Task1

---
+++ Lesson2/1/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+// создает рандомное число, от 10 до 99 и показывает какая цифра в нем макисмальная
+Console.Clear();
+int Number = new Random().Next(10,100);
+int Number1 = Number / 10;
+int Number2 = Number % 10;
+int Max = Number1;
+if (Number2>Number1) 
+Max = Number2; 
+Console.WriteLine($"В числе {Number} максимальная цифра {Max}");
