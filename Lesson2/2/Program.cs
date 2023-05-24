baa69994231fc47cb561bc14c63cc9171b74fc8a
Anton <B_atonyo_S@bk.ru>
2023-05-24 22:55:14 +0300

    Task2

---
+++ Lesson2/2/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+// /11 Напишите программу, которая выводит случайное
+// трёхзначное число и удаляет вторую цифру этого числа.
+Console.Clear();
+int Num = new Random().Next(100,1000);
+int Num1 = Num % 10;
+int Num2 = Num / 100;
+int Num3 = Num2*10+Num1;
+Console.WriteLine($"Было {Num} стало - {Num3}");
